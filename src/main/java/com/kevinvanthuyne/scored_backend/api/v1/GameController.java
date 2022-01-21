05ed907a7d2ecb726eabc86c442368facd2d9b30
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-01-21 22:01:45 +0100

    Allowed CORS for UI

--- src/main/java/com/kevinvanthuyne/scored_backend/api/v1/GameController.java
+++ src/main/java/com/kevinvanthuyne/scored_backend/api/v1/GameController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/v1/game")
