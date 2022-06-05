16bb075bf2a4160685aa41798a2ff29f8eb4d8cd
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-05 18:02:53 +0200

    Added a Setting entity

---
+++ src/main/java/com/kevinvanthuyne/agon_backend/model/Setting.java
@@ -0,0 +1,36 @@
+package com.kevinvanthuyne.agon_backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "settings")
+public class Setting {
+    @Id
+    private String key;
+    private String value;
+
+    public Setting(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Setting() {
+        this("", "");
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
