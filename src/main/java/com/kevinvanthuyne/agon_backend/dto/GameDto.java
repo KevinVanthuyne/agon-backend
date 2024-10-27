bffdf4afc57bb2511393125ed5582989ca01d211
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2024-10-27 20:05:03 +0100

    Added description to GameDto

--- src/main/java/com/kevinvanthuyne/agon_backend/dto/GameDto.java
+++ src/main/java/com/kevinvanthuyne/agon_backend/dto/GameDto.java
@@ -4,11 +4,13 @@ import com.kevinvanthuyne.agon_backend.model.Game;
 
 public record GameDto(int id,
                       String name,
+                      String description,
                       GameStyleDto gameStyle) implements ModelBuildable<Game> {
     public GameDto(Game game) {
         this(
                 game.getId(),
                 game.getName(),
+                game.getDescription(),
                 new GameStyleDto(game.getGameStyle())
         );
     }
